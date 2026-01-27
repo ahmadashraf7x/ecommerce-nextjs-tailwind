07771940dd59f0424a010587417f9b713da93236
Ahmad <ahmadashraf7x@gmail.com>
2026-01-27 20:35:40 +0200

    chore: remove unused hooks and loading component

--- components/ui/LoadingState.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingState() {
-  return (
-    <div className="flex items-center justify-center min-h-[300px]">
-      <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-500 border-t-transparent"></div>
-    </div>
-  );
-}
