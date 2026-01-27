1d447712fb7dffabf57220142a311c59468b3d0b
Ahmad <ahmadashraf7x@gmail.com>
2026-01-27 19:10:01 +0200

    feat: add not-found page for products details

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { getProductById } from "../../../services/products.service";
+import { notFound } from "next/navigation";
+
 import ProductDetailsClient from "./ProductDetailsClient";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
 
   const product = await getProductById(Number(id));
+
+   if (!product) {
+    notFound();
+  }
 
   return <ProductDetailsClient product={product} />;
 }
